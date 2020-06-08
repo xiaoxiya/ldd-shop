46c4452f842c154c075fe20b1998638370055da4
xiaoxiya <1292733831@qq.com>
2020-06-09 00:02:02 +0800

    init

---
+++ ldd-start/src/main/java/com/ldd/LddStartApplication.java
@@ -0,0 +1,13 @@
+package com.ldd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LddStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LddStartApplication.class, args);
+    }
+
+}
---
+++ ldd-start/src/test/java/com/ldd/LddStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ldd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LddStartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
